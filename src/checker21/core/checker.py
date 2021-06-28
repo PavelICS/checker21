02a3a7c30f57243b5cfe3dd5c33df7ca36bd3728
Pavel <pavel@yarpg.ru>
2021-06-28 16:43:11 +0300

    Added basic GitChecker

--- src/checker21/core/checker.py
+++ src/checker21/core/checker.py
@@ -1,5 +1,10 @@
 
-__all__ = ('Checker',)
+__all__ = ('Checker', 'GitChecker')
+
+import sys
+from pathlib import Path
+
+from checker21.utils.bash import bash
 
 
 class Checker:
@@ -7,6 +12,10 @@ class Checker:
 	name = None
 	verbose_name = None
 	description = None
+
+	def __init__(self, *, stdout=None, stderr=None):
+		self.stdout = stdout or sys.stdout
+		self.stderr = stderr or sys.stderr
 
 	def run(self, subject):
 		pass
@@ -18,5 +27,57 @@ class Checker:
 		return str(self.verbose_name or self.name)
 
 	def __repr__(self):
-		return f"{self.__class__}[{self}]"
+		return f"<{self.__class__}>[{self}]"
+
+
+class GitChecker(Checker):
+	git_url = None
+	target_dir = None
+
+	def __call__(self, subject):
+		assert self.git_url, "<GitChecker> should have `git_url` set"
+		self.git_clone()
+		super().__call__(subject)
+
+	def git_clone(self):
+		"""
+			Creates a git clone of the repository with checker
+			If the repository is cloned already, than it does nothing
+		"""
+		# check if the repository is cloned
+		if self.target_dir:
+			path = Path(self.target_dir)
+			if path.exists():
+				return
+
+		# clone the repository
+		cmd_args = ['git', 'clone', self.git_url]
+		if self.target_dir:
+			cmd_args.append(self.target_dir)
+		cmd = bash(cmd_args,
+		           stdout = self.stdout,
+		           stderr = self.stderr)
+
+	def git_clean(self):
+		"""
+			Deletes downloaded git files
+		"""
+		if self.git_is_ok_to_delete(self.target_dir):
+			bash(['rm', '-rf', self.target_dir],
+		           stdout = self.stdout,
+		           stderr = self.stderr)
+
+	def git_is_ok_to_delete(self, target_dir):
+		"""
+			Checks if path is relative
+		"""
+		if not target_dir:
+			return False
+		if target_dir == '.':
+			return False
+		if target_dir.starswith('..'):
+			return False
+		if target_dir.starswith('/'):
+			return False
+		return True
 
--- src/checker21/utils/bash.py
+++ src/checker21/utils/bash.py
@@ -2,15 +2,22 @@ import subprocess
 import sys
 
 
-def bash(command, *, echo=False):
+def bash(command, *, echo=True, stdout=None, stderr=None):
 	if echo:
 		if isinstance(command, str):
 			print(command)
 		else:
 			print(' '.join(command))
 
-	process = subprocess.run(command, capture_output=True)
-	sys.stderr.buffer.write(process.stderr)
+	options = {}
+	if stdout is None and stderr is None:
+		options['capture_output'] = True
+	else:
+		options['stdout'] = stdout
+		options['stderr'] = stderr
+
+	process = subprocess.run(command, **options)
+	# sys.stderr.buffer.write(process.stderr)
 	return process
 
 
